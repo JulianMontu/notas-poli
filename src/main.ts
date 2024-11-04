d7e262fc80b1f53e3a2daede380e3d398794b8ce
JulianMontu <julamontufar@misena.edu.co>
2024-11-03 19:04:22 -0500

    se realiza ajuste sobre swagger y se realiza contenerizacion de docker

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Notas poli')
+    .setDescription('Aplicacion de notas basada en todo list')
+    .setVersion('1.0')
+    .addTag('todo-list')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  app.getHttpAdapter().get('/', (req, res) => {
+    res.redirect('/api');
+  });
+  await app.listen(5000);
 }
 bootstrap();
